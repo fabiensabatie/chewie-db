bfb2f0079ac603ed3f713842a87330fb96f32028
Fabien Sabatié <fabien.sabatie@live.fr>
2024-07-11 20:20:55 +0200

    Update file

--- chewie1/index.tsx
+++ chewie1/index.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-import Scene from "./Scene";
+import Scene from "./App";
 import React from "react";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
