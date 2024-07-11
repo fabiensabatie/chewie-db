f35fdef76169d2090e00e1479e3c50b148f1219c
Fabien Sabatié <fabien.sabatie@live.fr>
2024-07-11 21:31:52 +0200

    [update]

--- chewie1/App.tsx
+++ chewie1/App.tsx
@@ -3,7 +3,6 @@ import { Box } from "./src/Box";
 export default function Scene() {
   return (
     <Canvas>
-      <OrbitControls />
       <ambientLight intensity={Math.PI / 2} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
