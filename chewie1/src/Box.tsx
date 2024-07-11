21414d22d0a70063bafc8151ca92f0e0474767d0
Fabien Sabatié <fabien.sabatie@live.fr>
2024-07-11 21:14:23 +0200

    [update]

--- chewie1/src/Box.tsx
+++ chewie1/src/Box.tsx
@@ -15,7 +15,8 @@ export function Box(props) {
     <mesh {...props}
       ref={meshRef}
       scale={active ? 1.5 : 1}
-      onClick={(event) => setActive(!active)} onPointerOver={(event) => setHover(true)} onPointerOut={(event) => setHover(false)}>
+      onClick={(event) => setActive(!active)}
+      onPointerOver={(event) => setHover(true)} onPointerOut={(event) => setHover(false)}>
       <boxGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={hovered ? "hotpink" : "red"} />
     </mesh>
