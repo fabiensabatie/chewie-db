a4ac31f43d571a51f59f2d274e93634673a68d3f
Fabien Sabatié <fabien.sabatie@live.fr>
2024-07-11 21:28:58 +0200

    [update]

--- chewie1/src/Box.tsx
+++ chewie1/src/Box.tsx
@@ -9,9 +9,7 @@ export function Box(props) {
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
   // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => {
-  meshRef.current.rotation.y += props.rotationSpeed * delta;
-});
+  useFrame((state, delta) => (meshRef.current.rotation.x += delta));
   // Return view, these are regular three.js elements expressed in JSX
   return (
     <mesh {...props}
