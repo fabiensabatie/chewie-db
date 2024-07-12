af08723b955b2e0506d39f2a3cb3241eb3fc9807
Fabien Sabatié <fabien.sabatie@live.fr>
2024-07-12 06:24:41 +0200

    [update]

--- chewie1/src/Box.tsx
+++ chewie1/src/Box.tsx
@@ -15,7 +15,7 @@ export function Box(props) {
   
   // Return view, these are regular three.js elements expressed in JSX
   return (
-    <mesh onPointerOver={(e) =>{ e.stopPropagation();}} onPointerOut={(e) => {}} {...props}
+    <mesh {...props}
       ref={meshRef}
       scale={active ? 1.5 : 1}
       onClick={(event) => {
