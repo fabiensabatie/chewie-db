8b5996c85d025e58c8ecf9592dae7051c02f38c7
Fabien Sabatié <fabien.sabatie@live.fr>
2024-07-12 07:07:15 +0200

    [update]

--- chewie1/src/Box.tsx
+++ chewie1/src/Box.tsx
@@ -25,7 +25,7 @@ export function Box(props) {
       onPointerOut={(event) => setHover(false)}
     >
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandard color={active ? props.clickedColor : (hovered ? props.hoveredColor : props.color)} />
+      <meshStandardMaterial color={active ? props.clickedColor : (hovered ? props.hoveredColor : props.color)} />
     </mesh>
   );
 }
