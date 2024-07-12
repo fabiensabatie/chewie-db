7f0237e63925f370f232921f30acb4c9321cfcf3
Fabien Sabatié <fabien.sabatie@live.fr>
2024-07-12 07:06:52 +0200

    [update]

--- chewie1/App.tsx
+++ chewie1/App.tsx
@@ -8,7 +8,7 @@ export default function Scene() {
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
       <Box position={[-1.2, 0, 0]} color="orange" hoveredColor="yellow" clickedColor="red" />
-      <Box position={[1.2, 0, 0]} />
+      <Box position={[-1.2, 0, 0]} color="orange" hoveredColor="yellow" clickedColor="red" />
     </Canvas>
   );
 }
