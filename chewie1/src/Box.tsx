bafed9b66a047dafd9ed71ea2d74fb93c55b71a3
Fabien Sabatié <fabien.sabatie@live.fr>
2024-07-12 05:32:23 +0200

    [update]

--- chewie1/src/Box.tsx
+++ chewie1/src/Box.tsx
@@ -19,7 +19,7 @@ export function Box(props) {
       scale={active ? 1.5 : 1}
       onClick={(event) => {
         setActive(!active);
-        window.Chewie.onComponentClicked("/src/Box.tsx")
+        window.Chewie.onComponentClicked("/src/Box.tsx");
       }}
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}
