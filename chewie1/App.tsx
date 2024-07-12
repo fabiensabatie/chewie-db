ce4d39d3e1e443b289b96446d46dc5de3378c42d
Fabien Sabatié <fabien.sabatie@live.fr>
2024-07-12 19:36:43 +0200

    [update]

--- chewie1/App.tsx
+++ chewie1/App.tsx
@@ -4,38 +4,34 @@ import { OrbitControls } from "@react-three/drei";
 import { Box } from "./src/Box";
 import { Sphere } from "./src/Sphere";
 import { Torus } from "./src/Torus";
-import Menu from "./src/Menu";
 
 export default function Scene() {
   return (
-    <div>
-      <Menu />
-      <Canvas>
-        <ambientLight intensity={Math.PI / 2} />
-        <spotLight
-          position={[10, 10, 10]}
-          angle={0.15}
-          penumbra={1}
-          decay={0}
-          intensity={Math.PI}
-        />
-        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-        <Box
-          position={[0, -2, 0]}
-          color="orange"
-          hoveredColor="yellow"
-          clickedColor="red"
-        />
-        <Box
-          position={[1.2, -2, 0]}
-          color="orange"
-          hoveredColor="yellow"
-          clickedColor="purple"
-        />
-        <Sphere />
-        <Torus position={[-1.2, -2, 0]} color="blue" hoveredColor="lightblue" clickedColor="darkblue" />
-        <OrbitControls />
-      </Canvas>
-    </div>
+    <Canvas>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        decay={0}
+        intensity={Math.PI}
+      />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <Box
+        position={[0, -2, 0]}
+        color="orange"
+        hoveredColor="yellow"
+        clickedColor="red"
+      />
+      <Box
+        position={[1.2, -2, 0]}
+        color="orange"
+        hoveredColor="yellow"
+        clickedColor="purple"
+      />
+      <Sphere />
+      <Torus position={[-1.2, -2, 0]} color="blue" hoveredColor="lightblue" clickedColor="darkblue" />
+      <OrbitControls />
+    </Canvas>
   );
 }
