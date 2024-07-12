52bf4e8708d1dddcd2b2a8fcecc11dcd16b7c6f7
Fabien Sabatié <fabien.sabatie@live.fr>
2024-07-12 07:16:44 +0200

    [update]

--- chewie1/App.tsx
+++ chewie1/App.tsx
@@ -10,7 +10,6 @@ export default function Scene() {
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
       <Box position={[-1.2, 0, 0]} color="orange" hoveredColor="yellow" clickedColor="red" />
       <Box position={[1.2, 0, 0]} color="orange" hoveredColor="yellow" clickedColor="red" />
-      <BouncingSphere />
       <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0,-1.0,0]}>
         <planeGeometry args={[100, 100]} />
         <meshStandardMaterial color={'dimgray'} />
