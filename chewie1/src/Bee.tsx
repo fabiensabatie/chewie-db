25e99efe6e68d1fd856458f194b077f9dff3d57b
Fabien Sabatié <fabien.sabatie@live.fr>
2024-07-12 19:43:22 +0200

    [update]

---
+++ chewie1/src/Bee.tsx
@@ -0,0 +1,40 @@
+import React, { useRef } from "react";
+import { useGLTF, useAnimations } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
+import { GLTF } from "three-stdlib";
+
+interface GLTFResult extends GLTF {
+  nodes: any;
+  materials: any;
+}
+
+const Bee = (props: JSX.IntrinsicElements["group"]) => {
+  const group = useRef<any>();
+  const { nodes, materials, animations } = useGLTF("/src/Bee.glb") as GLTFResult;
+  const { actions } = useAnimations(animations, group);
+
+  useFrame((state, delta) => {
+    if (group.current) {
+      group.current.rotation.y += delta;
+    }
+  });
+
+  React.useEffect(() => {
+    if (actions) {
+      actions["yourAnimationName"].play(); // Replace with the actual animation name from your GLB file
+    }
+  }, [actions]);
+
+  return (
+    <group ref={group} {...props} dispose={null}
+      onClick={(event) => {
+        window.Chewie.onComponentClicked("/src/Bee.tsx");
+      }}>
+      <primitive object={nodes.Scene} />
+    </group>
+  );
+};
+
+useGLTF.preload("/src/Bee.glb");
+
+export default Bee;
