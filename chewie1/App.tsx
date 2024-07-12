cfe7a1c1a5e3ebeff48410e50a2e61b39e5ef040
Fabien Sabatié <fabien.sabatie@live.fr>
2024-07-12 19:30:11 +0200

    [create]

--- chewie1/App.tsx
+++ chewie1/App.tsx
@@ -1,16 +1,34 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Box } from "./src/Box";
 import { Sphere } from "./src/Sphere";
+import { Torus } from "./src/Torus";
 
 export default function Scene() {
   return (
     <Canvas>
       <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        decay={0}
+        intensity={Math.PI}
+      />
       <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={[0, -2, 0]} color="orange" hoveredColor="yellow" clickedColor="red" />
-      <Box position={[1.2, -2, 0]} color="orange" hoveredColor="yellow" clickedColor="purple" />
+      <Box
+        position={[0, -2, 0]}
+        color="orange"
+        hoveredColor="yellow"
+        clickedColor="red"
+      />
+      <Box
+        position={[1.2, -2, 0]}
+        color="orange"
+        hoveredColor="yellow"
+        clickedColor="purple"
+      />
       <Sphere />
+      <Torus position={[-1.2, -2, 0]} color="blue" hoveredColor="lightblue" clickedColor="darkblue" />
     </Canvas>
   );
 }
