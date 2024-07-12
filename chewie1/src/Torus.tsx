fefbe33644693c1cb24476782ac3b4d25b95104c
Fabien Sabatié <fabien.sabatie@live.fr>
2024-07-12 19:30:55 +0200

    [update]

---
+++ chewie1/src/Torus.tsx
@@ -0,0 +1,33 @@
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+export function Torus(props) {
+  // This reference will give us direct access to the mesh
+  const meshRef = useRef();
+  // Set up state for the hovered and active state
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) => {
+    meshRef.current.rotation.x += delta * (active ? 2 : 1);
+  });
+  // Return view, these are regular three.js elements expressed in JSX
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={(event) => {
+        setActive(!active);
+        Add an
+      }}
+      onPointerOver={(event) => setHover(true)}
+      onPointerOut={(event) => setHover(false)}
+    >
+      <torusGeometry args={[1, 0.4, 16, 100]} />
+      <meshStandardMaterial
+        color={active ? props.clickedColor : hovered ? props.hoveredColor : props.color}
+      />
+    </mesh>
+  );
+}
