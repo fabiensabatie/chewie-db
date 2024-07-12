f743d3ccae1401e48ccc42e6549b1a666fb769f4
Fabien Sabatié <fabien.sabatie@live.fr>
2024-07-12 07:47:58 +0200

    [update]

--- chewie1/App.tsx
+++ chewie1/App.tsx
@@ -1,7 +1,6 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Box } from "./src/Box";
 import { Sphere } from "./src/Sphere";
-import { Floor } from "./src/Floor";
 
 export default function Scene() {
   return (
@@ -12,7 +11,6 @@ export default function Scene() {
       <Box position={[-1.2, 0, 0]} color="orange" hoveredColor="yellow" clickedColor="red" />
       <Box position={[1.2, 0, 0]} color="orange" hoveredColor="yellow" clickedColor="purple" />
       <Sphere />
-      <Floor />
     </Canvas>
   );
 }
