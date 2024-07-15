c7d4c39732e0a2d186db811666a70d21277727fc
Fabien Sabatié <fabien.sabatie@live.fr>
2024-07-15 21:24:53 +0200

    [update]

--- chewie1/App.tsx
+++ chewie1/App.tsx
@@ -35,7 +35,10 @@ export default function Scene() {
   return (
     <div style={{ width: '100vw', height: '100vh', position: 'relative' }}>
       <Canvas>
-        <SceneContent bullets={bullets} setBullets={setBullets} intervalRef={intervalRef} />
+         <ambientLight intensity={Math.PI / 2} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        <OrbitControls />
       </Canvas>
     </div>
   );
