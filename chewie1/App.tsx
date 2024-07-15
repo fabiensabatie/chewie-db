743188a7eb7962818ce7b0d9270224fd8c5c4810
Fabien Sabatié <fabien.sabatie@live.fr>
2024-07-15 19:09:38 +0200

    [create]

--- chewie1/App.tsx
+++ chewie1/App.tsx
@@ -13,7 +13,7 @@ export default function Scene() {
         <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
         <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
         <Box position={[0, -2, 0]} color="orange" hoveredColor="yellow" clickedColor="red" />
-        <Box position={[1.2, -2, 0]} color="orange" hoveredColor="yellow" clickedColor="purple" />
+        <Box position={[1.2, -2, 0]} color="orange" hoveredColor="yellow" clickedColor="green" />
         <Sphere />
         <Torus position={[-1.2, -2, 0]} color="blue" hoveredColor="lightblue" clickedColor="darkblue" />
         <OrbitControls />
