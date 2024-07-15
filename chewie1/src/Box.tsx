dcb0bc0c6ab09079611221b8ab41f23ddb88d04d
Fabien Sabatié <fabien.sabatie@live.fr>
2024-07-15 21:27:23 +0200

    [create]

--- chewie1/src/Box.tsx
+++ chewie1/src/Box.tsx
@@ -1,32 +1,13 @@
-import React, { useRef, useState } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
+import React from 'react';
+import { useFrame } from '@react-three/fiber';
 
-export function Box(props) {
-  // This reference will give us direct access to the mesh
-  
-  const meshRef = useRef();
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => {
-    meshRef.current.rotation.x += delta * (active ? 2 : 1);
-  });
-  // Return view, these are regular three.js elements expressed in JSX
+const Box = (props) => {
   return (
-    <mesh {...props}
-      ref={meshRef}
-      scale={active ? 1.5 : 1}
-      onClick={(event) => {
-        setActive(!active);
-        window.Chewie.onComponentClicked("/src/Box.tsx");
-      }}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}
-    >
+    <mesh {...props} onClick={(event) => {window.Chewie.onComponentClicked("/src/Box.tsx");}}>
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={active ? props.clickedColor : (hovered ? props.hoveredColor : props.color)} />
+      <meshStandardMaterial color={'red'} />
     </mesh>
   );
-}
-// coco
+};
+
+export { Box };
