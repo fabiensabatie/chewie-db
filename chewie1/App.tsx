d50e282080ec369e544f0b4e069ca1bd00a62414
Fabien Sabatié <fabien.sabatie@live.fr>
2024-07-15 21:25:37 +0200

    [update]

--- chewie1/App.tsx
+++ chewie1/App.tsx
@@ -7,6 +7,7 @@ export default function Scene() {
   return (
     <div style={{ width: '100vw', height: '100vh', position: 'relative' }}>
       <Canvas>
+        <Box position=[]
          <ambientLight intensity={Math.PI / 2} />
      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
         <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
