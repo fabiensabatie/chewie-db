a12af75c4b208946327b45583c0b9c1c17c5bcd3
Fabien Sabatié <fabien.sabatie@live.fr>
2024-07-15 19:08:34 +0200

    [create]

--- chewie1/src/Sphere.tsx
+++ chewie1/src/Sphere.tsx
@@ -7,11 +7,14 @@ export function Sphere(props) {
   // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
+  const [color, setColor] = useState('red');
+  
   // Subscribe this component to the render-loop, rotate the mesh every frame
   useFrame((state, delta) => {
     meshRef.current.rotation.y += delta * (active ? 2 : 1);
     meshRef.current.position.y = Math.abs(Math.sin(state.clock.elapsedTime * (active ? 3 : 2))) * 2;
   });
+  
   // Return view, these are regular three.js elements expressed in JSX
   return (
     <mesh {...props}
@@ -19,13 +22,14 @@ export function Sphere(props) {
       scale={active ? 1.5 : 1}
       onClick={(event) => {
         setActive(!active);
+        setColor('green'); // Set color to green when clicked
         window.Chewie.onComponentClicked("/src/Sphere.tsx");
       }}
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}
     >
       <sphereGeometry args={[1, 32, 32]} />
-      <meshStandardMaterial color={hovered ? "purple" : "red"} />
+      <meshStandardMaterial color={hovered ? "purple" : color} />
     </mesh>
   );
 }
