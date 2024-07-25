39b464c3f10e622a8c5052d08040939b2a01afa9
Fabien Sabatié <fabien.sabatie@live.fr>
2024-07-25 19:26:20 +0200

    [update]

--- chewie1/App.tsx
+++ chewie1/App.tsx
@@ -9,7 +9,6 @@ export default function Scene() {
       <Canvas>
         <Box position={[0,0,0]} />
          <ambientLight intensity={Math.PI / 2} />
-     <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
         <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
         <OrbitControls />
       </Canvas>
