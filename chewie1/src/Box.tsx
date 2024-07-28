149b7081170d204ead98566c5c1341f8d6a0a367
Fabien Sabatié <fabien.sabatie@live.fr>
2024-07-28 22:26:17 +0200

    [update]

--- chewie1/src/Box.tsx
+++ chewie1/src/Box.tsx
@@ -15,6 +15,8 @@ export function Box(props) {
     meshRef.current.rotation.y += delta * (active ? 2 : 1);
     meshRef.current.position.y = Math.abs(Math.sin(state.clock.elapsedTime * (active ? 3 : 2))) * 2;
   });
+
+  return (
     <mesh {...props}
       ref={meshRef}
       scale={active ? 1.5 : 1}
